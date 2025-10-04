41c1672de4e47f50316b4b656c5becbeae46f74d
Atishey Jain <atisheyjain216@gmail.com>
2025-10-04 10:13:11 +0530

    update

---
+++ JavaScript/Day04/index.js
@@ -0,0 +1,8 @@
+// console.log(2+5);
+// console.log(2*5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(2**5);
+// console.log(2/5);
+// console.log(2%5);
+console.log(null==undefined)
